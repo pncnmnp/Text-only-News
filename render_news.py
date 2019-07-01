f9fa06272a6abee576bebeb882f1da3b35d4d2d2
parthparikh <parthparikh1999p@gmail.com>
2019-07-01 16:47:28 +0530

    Added site disclaimers

--- render_news.py
+++ render_news.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import get_news
+from urllib.parse import urlparse
 
 app = Flask(__name__)
 parsed = get_news.ParseNews()
@@ -15,8 +16,10 @@ def display_headlines():
 def display_summary():
 	title = request.args['title']
 	news = parsed.fetch_news()
+	site_name = urlparse(news[title]['url']).hostname.replace('feeds.', '')
 	return render_template('summary.html', 
 							title=title, 
 							summary=news[title]['summary'].split('\n'),
 							link=news[title]['url'],
-							published=news[title]['published'])
+							published=news[title]['published'],
+							site_name=site_name)
