4353475dad0bd7a6320401ac74085694cd5e346b
parthparikh <parthparikh1999p@gmail.com>
2019-07-19 05:47:12 +0530

    - Service worker will now update and refresh for latest news

--- static/service-worker.js
+++ static/service-worker.js
@@ -19,14 +19,40 @@ self.addEventListener('activate', event => {
 	console.log('Service worker activate event!');
 });
 
-self.addEventListener('fetch', event => {
+// The below event listener is from 
+// https://serviceworke.rs/strategy-cache-update-and-refresh_service-worker_doc.html
+
+self.addEventListener('fetch', function(event) {
 	console.log('Fetch intercepted for:', event.request.url);
-	event.respondWith(caches.match(event.request)
-		.then(cachedResponse => {
-				if (cachedResponse) {
-					return cachedResponse;
-				}
-				return fetch(event.request);
-			})
-		);
+	event.respondWith(fromCache(event.request));
+	event.waitUntil(update(event.request).then(refresh));
 });
+
+function fromCache(request) {
+	return caches.open(cacheName).then(function (cache) {
+		return cache.match(request);
+	});
+}
+
+function update(request) {
+	return caches.open(cacheName).then(function (cache) {
+		return fetch(request).then(function (response) {
+			return cache.put(request, response.clone()).then(function () {
+				return response;
+			});
+		});
+	});
+}
+
+function refresh(response) {
+	return self.clients.matchAll().then(function (clients) {
+		clients.forEach(function (client) {
+			var message = {
+				type: 'refresh',
+				url: response.url,
+				eTag: response.headers.get('ETag')
+			};
+			client.postMessage(JSON.stringify(message));
+		});
+	});
+}
